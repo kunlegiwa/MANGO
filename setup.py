cc64e13aa6312abf6b8966632846dac474a19c12
kunlegiwa <olakunlegiwa@gmail.com>
2020-09-15 13:04:07 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MangoTest',
+    version='1',
+    description='Mango Solutions Distributions Package',
+    url='https://github.com/kunlegiwa/MANGO.git',
+    author='Kunle Giwa',
+    author_email='olakunlegiwa@gmail.com',
+    license='unlicense',
+    packages=setuptools.find_packages(),
+    
+)
