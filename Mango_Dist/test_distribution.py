149401803685232ddb1e81b4a3f9eccba1fee3c0
kunlegiwa <olakunlegiwa@gmail.com>
2020-09-16 21:56:21 +0100

    No size given

--- Mango_Dist/test_distribution.py
+++ Mango_Dist/test_distribution.py
@@ -6,7 +6,7 @@ print(df.draw('lognormal', size=5, mean=4, sigma=2))
 
 dist1 = Distribution(size=3)
 print(dist1.draw())
-dist1.size = 6
+dist1.size = None
 print(dist1.draw())
 print(dist1.summarise())
 
